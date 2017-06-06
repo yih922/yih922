fcc671ebf945d98b117bbbbcdb544809f85d5888
eric huang <jpgogo@msn.com>
2017-06-06 11:50:28 +0800

    implemet the getprice() method

--- HarryPotter.Services/HarryPotterBook.cs
+++ HarryPotter.Services/HarryPotterBook.cs
@@ -36,8 +36,46 @@ namespace HarryPotter.Services
     {
         public static double GetPrice(this IEnumerable<HarryPotterBook> books)
         {
-           
-            return 0;
+            double totalPrice = 0;
+
+            books = books.ToLookup(p => p.Series)
+                         .Select(p => new HarryPotterBook(p.Key, p.Sum(g => g.Quantity)))
+                         .Where(p => p.Quantity > 0).ToList();
+
+            while (books.Count() > 0)
+            {
+                var minbookQty = books.Where(p => p.Quantity > 0).Min(p => p.Quantity);
+                var bookseriesCount = books.Count();
+
+                totalPrice += minbookQty * HarryPotterBook.BookPrice * bookseriesCount * GetDiscount(bookseriesCount);
+
+
+                foreach (var book in books)
+                {
+                    book.Quantity = book.Quantity - minbookQty;
+                }
+                books = books.Where(p => p.Quantity > 0).ToList();
+            }
+            return totalPrice;
+        }
+
+        private static double GetDiscount(int bookseriesCount)
+        {
+            switch (bookseriesCount)
+            {
+                case 1:
+                    return 1.0;
+                case 2:
+                    return 0.95;
+                case 3:
+                    return 0.9;
+                case 4:
+                    return 0.8;
+                case 5:
+                    return 0.75;
+                default:
+                    return 0;
+            }
         }
     }
 }
