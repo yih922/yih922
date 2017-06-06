40096f034f9acff427521ca1cb5cbfcd2b62df59
eric huang <jpgogo@msn.com>
2017-06-06 11:48:34 +0800

    Implement the step desciption

---
+++ HarryPotter.Services/HarryPotterBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Services
+{
+    public class HarryPotterBook
+    {
+        public static double BookPrice => 100;
+
+        private Series _series;
+
+        public HarryPotterBook(Series series, int quantity)
+        {
+            Quantity = quantity;
+            _series = series;
+        }
+
+        //properties
+        public Series Series => _series;
+        public int Quantity { get; set; }
+    }
+
+    public enum Series
+    {
+        First,
+        Second,
+        Third,
+        Forth,
+        Fifth
+    }
+
+    public static class Extension
+    {
+        public static double GetPrice(this IEnumerable<HarryPotterBook> books)
+        {
+           
+            return 0;
+        }
+    }
+}
--- HarryPotter.Tests/HerryPorterBookTests.cs
+++ HarryPotter.Tests/HerryPorterBookTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
+using HarryPotter.Services;
 
 namespace HarryPotter.Tests
 {
@@ -8,13 +9,27 @@ namespace HarryPotter.Tests
     public class HerryPorterBookTests
     {
         [TestMethod]
-        public void Should_Get_Correct_Price_For_Nonduplicate_Booklist()
+        [DataRow(1, 0, 0, 0, 0, 100)]
+        [DataRow(1, 1, 0, 0, 0, 190)]
+        [DataRow(1, 1, 1, 0, 0, 270)]
+        [DataRow(1, 1, 1, 1, 0, 320)]
+        [DataRow(1, 1, 1, 1, 1, 375)]
+        [DataRow(1, 1, 2, 0, 0, 370)]
+        [DataRow(1, 2, 2, 0, 0, 460)]
+        public void Should_Get_Correct_Price_For_Nonduplicate_Booklist(int FirstQty, int SecondQty, int ThirdQty, int ForthQty, int FifthQty, double expected)
         {
-            //new a harry potter book list
+            var hpbooks = new HarryPotterBook[]
+                   {
+                       new HarryPotterBook(Series.First, FirstQty),
+                       new HarryPotterBook(Series.Second, SecondQty),
+                       new HarryPotterBook(Series.Third, ThirdQty),
+                       new HarryPotterBook(Series.Forth, ForthQty),
+                       new HarryPotterBook(Series.Fifth, FifthQty)
+                   };
 
-            //get price of the book list
+            var result = hpbooks.GetPrice();
 
-            //examine the price
+            result.Should().Be(expected, result.ToString());
         }
     }
 }
